286ab2bf40289a4be0e060ea86a69c03d5525f33
Mia <mansm@sas.upenn.edu>
2024-04-01 12:55:14 -0400

    adjusted error in brackets

--- sec3part1/iframeparser.js
+++ sec3part1/iframeparser.js
@@ -9,50 +9,52 @@ for (let i = 0; i < iframes.length; i++) {
 
     if (url.startsWith("https://docs.google.com/spreadsheets/d/")) {
         // Get all iframes that were parsed before this tag
-var iframes = document.getElementsByTagName("iframe");
+        var iframes = document.getElementsByTagName("iframe");
 
-for (let i = 0; i < iframes.length; i++) {
-    var url = iframes[i].getAttribute("src");
-    if (url.startsWith("https://docs.google.com/spreadsheets/d/")) {
-        // create div and replace iframe
-        let d = document.createElement('div');
-        d.classList.add("embedded-spreadsheet"); // optional
-        iframes[i].parentElement.replaceChild(d, iframes[i]);
+        for (let i = 0; i < iframes.length; i++) {
+            var url = iframes[i].getAttribute("src");
+            if (url.startsWith("https://docs.google.com/spreadsheets/d/")) {
+                // create div and replace iframe
+                let d = document.createElement('div');
+                d.classList.add("embedded-spreadsheet"); // optional
+                iframes[i].parentElement.replaceChild(d, iframes[i]);
 
-        // Extract the spreadsheet ID
-        var docId = url.split("/")[5];
+                // Extract the spreadsheet ID
+                var docId = url.split("/")[5];
 
-        // Create Google Sheets API URL
-        var sheetsApiUrl = "https://sheets.googleapis.com/v4/spreadsheets/" + docId + "/values/Sheet1?alt=json";
+                // Create Google Sheets API URL
+                var sheetsApiUrl = "https://sheets.googleapis.com/v4/spreadsheets/" + docId + "/values/Sheet1?alt=json";
 
-        // CORS request
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', sheetsApiUrl, true);
-        xhr.onload = function() {
-            // Check if request was successful
-            if (xhr.status >= 200 && xhr.status < 300) {
-                // Parse JSON response
-                var response = JSON.parse(xhr.responseText);
-                var values = response.values;
+                // CORS request
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', sheetsApiUrl, true);
+                xhr.onload = function() {
+                    // Check if request was successful
+                    if (xhr.status >= 200 && xhr.status < 300) {
+                        // Parse JSON response
+                        var response = JSON.parse(xhr.responseText);
+                        var values = response.values;
 
-                // Create table HTML
-                var tableHtml = "<table>";
-                for (var j = 0; j < values.length; j++) {
-                    tableHtml += "<tr>";
-                    for (var k = 0; k < values[j].length; k++) {
-                        tableHtml += "<td>" + values[j][k] + "</td>";
+                        // Create table HTML
+                        var tableHtml = "<table>";
+                        for (var j = 0; j < values.length; j++) {
+                            tableHtml += "<tr>";
+                            for (var k = 0; k < values[j].length; k++) {
+                                tableHtml += "<td>" + values[j][k] + "</td>";
+                            }
+                            tableHtml += "</tr>";
+                        }
+                        tableHtml += "</table>";
+
+                        // Display table in the div
+                        d.innerHTML = tableHtml;
+                    } else {
+                        console.error("Failed to fetch spreadsheet data. Status code: " + xhr.status);
                     }
-                    tableHtml += "</tr>";
-                }
-                tableHtml += "</table>";
-
-                // Display table in the div
-                d.innerHTML = tableHtml;
-            } else {
-                console.error("Failed to fetch spreadsheet data. Status code: " + xhr.status);
+                };
+                xhr.send();
             }
-        };
-        xhr.send();
+        }
     }
 }
 
